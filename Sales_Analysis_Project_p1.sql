5dc9911297054e6df1c77bdb0092d3f0efe579e8
shubham kumar <116440288+gwarishubham01@users.noreply.github.com>
2025-04-07 21:59:55 +0530

    Update Sales_Analysis_Project_p1.sql

--- Sales_Analysis_Project_p1.sql
+++ Sales_Analysis_Project_p1.sql
@@ -10,16 +10,22 @@ CREATE TABLE retail_sales
 (
 		transactions_id	INT Primary KEY,
 		sale_date	DATE,
-		sale_time 	TIME,
+		sale_time TIME,
 		customer_id	INT,
-		gender 		VARCHAR(15),	
-		age 		INT,	
-		category 	VARCHAR(15),	
-		quantiy		INT,
+		gender VARCHAR(15),	
+		age INT,	
+		category VARCHAR(15),	
+		quantiy	INT,
 		price_per_unit	FLOAT,
-		cogs 		FLOAT,	
-		total_sale 	FLOAT
+		cogs FLOAT,	
+		total_sale FLOAT
 );
+
+-- Data Exploration & CLeaning
+-- How many sales we have?
+SELECT COUNT(*) as total_sales FROM retail_sales;
+SELECT COUNT(DISTINCT customer_id) as total_sales FROM retail_sales;
+SELECT DISTINCT category FROM retail_sales;
 
 SELECT * FROM retail_sales
 WHERE 
@@ -33,75 +39,75 @@ WHERE
 	gender is NULL or category is NULL or 
 	quantity is NULL or cogs is NULL or total_sale is NULL;
 
--- Data Exploration
--- How many sales we have?
-SELECT COUNT(*) as total_sales FROM retail_sales
-SELECT COUNT(DISTINCT customer_id) as total_sales FROM retail_sales
-SELECT DISTINCT category FROM retail_sales
 
 -- Data Analysis & Business key Problems & Answers
 
+--The following queries were developed to answer specific business questions:
+
 -- Qns1: Write a SQL query to retrieve all columns for sales moade on "2022-11-05".
 
-SELECT * FROM retail_sales
-WHERE 
-	sale_date = '2022-11-05';
+SELECT *
+FROM retail_sales
+WHERE sale_date = '2022-11-05';
 
 -- Qns2: Write a SQl query to retrieve all transactions where the category is 'clothing' and the quantity said is more than or equal to 4 in the month of Nov- 2022.
 
 SELECT * FROM retail_sales
 WHERE 
-	category = 'Clothing' and
-	quantity >= 4 and 
+	category = 'Clothing' 
+	AND
+	quantity >= 4 
+	AND
 	TO_CHAR(sale_date, 'YYYY-MM') = '2022-11';
 
 -- Qns3: Write a SQL query to calculate the total sales for each category.
 
 SELECT
 	category, 
-	SUM(total_sale) as net_sales,
+	SUM(total_sale) as net_sale,
 	COUNT(*) as total_orders
 FROM retail_sales
-GROUP BY 1
+GROUP BY 1;
 
 -- Qns4: Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
 
 SELECT 
-	ROUND(AVG(age), 2) as average_age 
+	ROUND(AVG(age),2 ) as average_age 
 FROM retail_sales
-WHERE 
-	category = 'Beauty';
+WHERE category = 'Beauty';
 
 -- Qns5: Write a SQL query to find all transactions where the total_sale is greater than 1000.
 
 SELECT * FROM retail_sales
-WHERE 
-	total_sale>1000;
+WHERE total_sale>1000;
 
 -- Qns6: Write a SQL query to find the total number of transactions made by each gender in each category.
 
 SELECT 
 	category, 
-	gender, 
+	gender,  
 	COUNT(total_sale) as total_number_of_transactions
 FROM retail_sales
-group by category, gender;
+group by 	
+	category, 
+	gender
+ORDER BY 1;
 
 -- Qns7: Write a SQL query to calculate the average sale for each month. Find out best selling month in each year.
 
 SELECT 
-       year,
-       month,
-    avg_sale
-FROM 
-(    
-SELECT 
-    EXTRACT(YEAR FROM sale_date) as year,
-    EXTRACT(MONTH FROM sale_date) as month,
-    AVG(total_sale) as avg_sale,
-    RANK() OVER(PARTITION BY EXTRACT(YEAR FROM sale_date) ORDER BY AVG(total_sale) DESC) as rank
-FROM retail_sales
-GROUP BY 1, 2
+year,
+month,
+avg_sale
+FROM
+(
+SELECT
+	EXTRACT(YEAR FROM sale_date) as Year,
+	EXTRACT(MONTH FROM sale_date) as Month, 
+	AVG(total_sale) as total_sale,
+	RANK() OVER(PARTITION BY EXTRACT(YEAR FROM sale_date) ORDER BY AVG(total_sale) DESC) as rank
+FROM retail_sales 
+GROUP BY 1,2
 ) as t1
 WHERE rank = 1
 
@@ -109,7 +115,8 @@ WHERE rank = 1
 
 SELECT 
 	customer_id, 
-	SUM(total_sale) as Total_sales FROM retail_sales
+	SUM(total_sale) as Total_sales 
+FROM retail_sales
 GROUP BY 1
 ORDER BY 2 DESC
 LIMIT 5;
@@ -118,15 +125,25 @@ LIMIT 5;
 
 SELECT  
 	category,
-	COUNT(DISTINCT customer_id) FROM retail_sales
-group by 1;
+	COUNT(DISTINCT customer_id) 
+FROM retail_sales
+group by category;
 
 -- Qns10: Write a SQL query to create each shift and number of orders (Example Morning <=12, Afternoon Between 12 & 17, evening >17).
 
+WITH hourly_sale
+AS
+(
 SELECT *,
-	CASE
-	WHEN EXTRACT(HOUR FROM sale_time) <= 12 THEN 'Morning'
-	WHEN EXTRACT(HOUR FROM sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
-	ELSE 'Evening'
-	END as shift	
+    CASE
+        WHEN EXTRACT(HOUR FROM sale_time) < 12 THEN 'Morning'
+        WHEN EXTRACT(HOUR FROM sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+        ELSE 'Evening'
+    END as shift
 FROM retail_sales
+)
+SELECT 
+    shift,
+    COUNT(*) as total_orders    
+FROM hourly_sale
+GROUP BY shift;

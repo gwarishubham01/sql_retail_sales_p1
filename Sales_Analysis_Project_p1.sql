7f43dd4515f54c731f1397b83df56925d1072929
shubham kumar <116440288+gwarishubham01@users.noreply.github.com>
2025-04-07 19:21:46 +0530

    Update Sales_Analysis_Project_p1.sql

--- Sales_Analysis_Project_p1.sql
+++ Sales_Analysis_Project_p1.sql
@@ -10,15 +10,15 @@ CREATE TABLE retail_sales
 (
 		transactions_id	INT Primary KEY,
 		sale_date	DATE,
-		sale_time TIME,
+		sale_time 	TIME,
 		customer_id	INT,
-		gender VARCHAR(15),	
-		age INT,	
-		category VARCHAR(15),	
-		quantiy	INT,
+		gender 		VARCHAR(15),	
+		age 		INT,	
+		category 	VARCHAR(15),	
+		quantiy		INT,
 		price_per_unit	FLOAT,
-		cogs FLOAT,	
-		total_sale FLOAT
+		cogs 		FLOAT,	
+		total_sale 	FLOAT
 );
 
 SELECT * FROM retail_sales
@@ -34,7 +34,6 @@ WHERE
 	quantity is NULL or cogs is NULL or total_sale is NULL;
 
 -- Data Exploration
-
 -- How many sales we have?
 SELECT COUNT(*) as total_sales FROM retail_sales
 
@@ -70,33 +69,36 @@ GROUP BY 1
 -- Qns4: Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
 
 SELECT ROUND(AVG(age),2 ) as average_age FROM retail_sales
-WHERE 
-category = 'Beauty';
+WHERE category = 'Beauty';
 
 -- Qns5: Write a SQL query to find all transactions where the total_sale is greater than 1000.
 
 SELECT * FROM retail_sales
-WHERE
-	total_sale>1000;
+WHERE total_sale>1000;
 
 -- Qns6: Write a SQL query to find the total number of transactions made by each gender in each category.
 
-SELECT category, gender,  
-		COUNT(total_sale) as total_number_of_transactions
-FROM 
-	retail_sales
-group by 	
-	category, gender;
+SELECT category, gender, COUNT(total_sale) as total_number_of_transactions
+FROM retail_sales
+group by category, gender;
 
 -- Qns7: Write a SQL query to calculate the average sale for each month. Find out best selling month in each year.
 
 SELECT 
-	EXTRACT(YEAR FROM sale_date) as Year,
-	EXTRACT(MONTH FROM sale_date) as Month, 
-	AVG(total_sale) as total_sale 
-FROM retail_sales 
-GROUP BY 1,2
-ORDER BY 1, 3 DESC;
+       year,
+       month,
+    avg_sale
+FROM 
+(    
+SELECT 
+    EXTRACT(YEAR FROM sale_date) as year,
+    EXTRACT(MONTH FROM sale_date) as month,
+    AVG(total_sale) as avg_sale,
+    RANK() OVER(PARTITION BY EXTRACT(YEAR FROM sale_date) ORDER BY AVG(total_sale) DESC) as rank
+FROM retail_sales
+GROUP BY 1, 2
+) as t1
+WHERE rank = 1
 
 -- Qns8: Write a SQL query find the top 5 customers based on the highest total sales.
 
